8f4e12fcd5ef15346b664a463cdd20b0e8c1a65a
Raphael Goutmann <raphaelgoutmann@gmail.com>
2023-08-03 08:08:06 +0200

    Add responsive menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-/* nothing for the moment */
+let toggleNav = document.querySelector(".toggle-nav-btn")
+
+if (toggleNav)
+{
+    toggleNav.addEventListener("click", (e) =>
+    {
+        let responsiveNav = document.querySelector(".responsive-nav")
+        if(responsiveNav)
+        {
+            responsiveNav.classList.toggle("active")
+            toggleNav.classList.toggle("active")
+        }
+
+    })
+}
