cdabb93674a23b18110772399a2f6cb0ce1e9d03
Welpike <welpike@protonmail.com>
2023-08-08 19:55:30 +0200

    add banner (dev in progress)

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,23 @@ if (toggleNav)
 
     })
 }
+
+// ------------------------------------------------------------------------------------
+// "website in developpment" banner - @Welpike
+// ------------------------------------------------------------------------------------
+/**
+ * Create element from a string (for elements contained in a string)
+ * @param {string} str The string with HTML in it
+ * @returns {ChildNode}
+ */
+Document.prototype.createElementFromString = function (str) {
+    const element = new DOMParser().parseFromString(str, 'text/html')
+    return element.documentElement.querySelector('body').firstChild
+};
+
+let bannerHTML = `<div id="banner"><p>This project is under development. You can follow the evolution of it on <a href="https://github.com/paxo-rch">GitHub</a>.</p></div>`,
+    banner = document.createElementFromString(bannerHTML)
+
+document.body.prepend(banner)
+document.body.style.position = "relative"
+document.querySelector("header").style.marginTop += banner.offsetHeight + "px"
