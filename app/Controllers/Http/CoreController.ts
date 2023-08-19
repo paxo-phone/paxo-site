5fb544b47ad62e408779e5065a879213cb5648cb
Welpike <welpike@protonmail.com>
2023-08-19 11:43:58 +0200

    configure db (sqlite & mariadb) + add auth (updates needed)

--- app/Controllers/Http/CoreController.ts
+++ app/Controllers/Http/CoreController.ts
@@ -1,15 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class CoreController {
-  async index({ view }: HttpContextContract) {
+  public async index({ view }: HttpContextContract) {
     return view.render('core/index')
   }
 
-  async contact({ view }: HttpContextContract) {
+  public async contact({ view }: HttpContextContract) {
     return view.render('core/contact')
   }
 
-  async contribute({ view }: HttpContextContract) {
+  public async contribute({ view }: HttpContextContract) {
     return view.render('core/contribute')
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -25,3 +25,20 @@ Route.group(() => {
   Route.get('/contact', 'CoreController.contact')
   Route.get('/contribute', 'CoreController.contribute')
 }).prefix('/')
+
+Route.group(() => {
+  Route.get('/register', 'UsersController.register')
+    .as('users.register')
+  Route.post('/register', 'UsersController.store')
+
+  Route.get('/login', 'UsersController.login')
+    .as('users.login')
+  Route.post('/login', 'UsersController.loginProcess')
+
+  Route.post('/logout', 'UsersController.logoutProcess')
+    .as('users.logoutProcess')
+
+  Route.get('/dashboard', 'UsersController.dashboard')
+    .middleware('auth')
+    .as('users.dashboard')
+}).prefix('/users')
