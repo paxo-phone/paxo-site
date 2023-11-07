a95fe5dd2db547160b9114d50c1b38cf20e04e73
DarkBrines <darkbrinees@gmail.com>
2023-11-07 19:58:54 +0100

    Final ehencements

--- app/Controllers/Http/TutorialsController.ts
+++ app/Controllers/Http/TutorialsController.ts
@@ -3,10 +3,10 @@ import Tutorial from 'App/Models/Tutorial'
 
 export default class TutorialsController {
   public async index ({ view, request }: HttpContextContract) {
-    const tutorials = await Tutorial.query().paginate(request.input('page', 0), 5)
+    const tutorials = await Tutorial.query().paginate(request.input('page'), 5)
 
     return view.render('tutorials/index', {
-      tutorials: tutorials,
+      tutorials,
     })
   }
 
