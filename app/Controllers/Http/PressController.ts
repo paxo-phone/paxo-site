d9c459c2f5d7daa5be5d0d8872bd97d8429305e5
DarkBrines <darkbrinees@gmail.com>
2023-11-19 21:11:32 +0100

    Edited press page design

--- app/Controllers/Http/PressController.ts
+++ app/Controllers/Http/PressController.ts
@@ -3,7 +3,7 @@ import PressArticle from "App/Models/PressArticle"
 
 export default class PressController {
   public async index({ view }: HttpContextContract) {
-    const pressArticles = await PressArticle.query().paginate(1)
+    const pressArticles = await PressArticle.query().exec()
 
     return view.render('press/index', {
       pressArticles: pressArticles
