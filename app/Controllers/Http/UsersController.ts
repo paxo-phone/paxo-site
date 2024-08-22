c65a748ddd1c9ff4db60c8062bd593f1b38a1981
SuperMax <141626084+iamsupermax@users.noreply.github.com>
2024-08-22 18:59:05 +0200

    leave in comment UsersController.ts associated to github

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,62 +5,62 @@ import { App, Octokit } from 'octokit';
 import { readFileSync } from 'node:fs';
 import JWT from '../../lib/jwt';
 
-export const authorization_url = "https://github.com/login/oauth/authorize"
-  + "?redirect_uri=" + encodeURIComponent(process.env.ACCESS_ADDRESS + "/auth/callback/")
-  + "&client_id=" + process.env.GITHUB_CLIENT_ID
+//export const authorization_url = "https://github.com/login/oauth/authorize"
+//  + "?redirect_uri=" + encodeURIComponent(process.env.ACCESS_ADDRESS + "/auth/callback/")
+//  + "&client_id=" + process.env.GITHUB_CLIENT_ID
 
-const githubApp = new App({
-  appId: process.env.GITHUB_APP_ID ?? "",
-  privateKey: process.env.GITHUB_APP_PRIVATE_KEY ?? readFileSync(process.env.GITHUB_APP_PRIVATE_KEY_PATH ?? "").toString(),
-  oauth: {
-    clientId: process.env.GITHUB_CLIENT_ID ?? "",
-    clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ""
-  },
-});
+//const githubApp = new App({
+//  appId: process.env.GITHUB_APP_ID ?? "",
+//  privateKey: process.env.GITHUB_APP_PRIVATE_KEY ?? readFileSync(process.env.GITHUB_APP_PRIVATE_KEY_PATH ?? "").toString(),
+//  oauth: {
+//    clientId: process.env.GITHUB_CLIENT_ID ?? "",
+//    clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ""
+//  },
+//});
 
-export default class UsersController {
-  public async login({ response }: HttpContextContract) {
-    return response.redirect(authorization_url)
-  }
+//export default class UsersController {
+//  public async login({ response }: HttpContextContract) {
+//    return response.redirect(authorization_url)
+//  }
 
-  public async callback({ request, auth, response, view }: HttpContextContract) {
-    const data = await request.validate({
-      schema: schema.create({
-        code: schema.string()
-      }),
-      messages: {
-        '*': "The authentication service returned an error"
-      }
-    })
+//  public async callback({ request, auth, response, view }: HttpContextContract) {
+//    const data = await request.validate({
+//      schema: schema.create({
+//        code: schema.string()
+//      }),
+//      messages: {
+//        '*': "The authentication service returned an error"
+//      }
+//    })
 
-    const { authentication: token } = await githubApp.oauth.createToken({
-      code: data.code
-    })
-    const octokit = new Octokit({
-      auth: token.token
-    })
+//    const { authentication: token } = await githubApp.oauth.createToken({
+//      code: data.code
+//    })
+//    const octokit = new Octokit({
+//      auth: token.token
+//    })
 
-    const { data: userdata } = await octokit.rest.users.getAuthenticated()
+//    const { data: userdata } = await octokit.rest.users.getAuthenticated()
 
-    const user = await User.query()
-      .where('id', userdata.id)
-      .first()
+//    const user = await User.query()
+//      .where('id', userdata.id)
+//      .first()
 
-    if (!user) {
-      const { data: emaildata } = await octokit.rest.users.listEmailsForAuthenticatedUser()
-      let email: string | undefined = undefined
-      for (const em of emaildata) {
-        if (em.primary) {
-          if (em.verified) email = em.email
-          break
-        }
-      }
+//    if (!user) {
+//      const { data: emaildata } = await octokit.rest.users.listEmailsForAuthenticatedUser()
+//      let email: string | undefined = undefined
+//      for (const em of emaildata) {
+//        if (em.primary) {
+//          if (em.verified) email = em.email
+//          break
+//        }
+//      }
 
-      const user_object = {
-        id: userdata.id,
-        name: userdata.name ?? userdata.login,
-        email
-      }
+//      const user_object = {
+//        id: userdata.id,
+//        name: userdata.name ?? userdata.login,
+//        email
+//      }
 
       // For controlled registration
       // return view.render('auth/post_register', {
@@ -68,17 +68,18 @@ export default class UsersController {
       // })
 
       // For free registration
-      const user = await User.create(user_object)
-      await user.save()
-    }
+//      const user = await User.create(user_object)
+//      await user.save()
+//    }
 
-    await auth.use('web').loginViaId(userdata.id)
-    return response.redirect().toRoute("apps")
-  }
+//    await auth.use('web').loginViaId(userdata.id)
+//    return response.redirect().toRoute("apps")
+//  }
 
-  public async logout({ auth, response, session }: HttpContextContract) {
-    await auth.use('web').logout()
-    session.flash({ success: 'Log out successfully' })
-    response.redirect().toRoute('apps')
-  }
-}
+//  public async logout({ auth, response, session }: HttpContextContract) {
+//    await auth.use('web').logout()
+//    session.flash({ success: 'Log out successfully' })
+//    response.redirect().toRoute('apps')
+// }
+//}
+
