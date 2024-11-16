edce1fd40b0a054bbcaa84f2ab6305030f386a39
Raphael Roumezin <raphael.roumezin@ik.me>
2024-11-16 11:55:22 +0100

    Removed debugging artifacts

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -2,19 +2,14 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import App from "App/Models/App"
 
 export default class DashboardController {
-    public async index({ auth, view }: HttpContextContract) {
-        const user = auth.use('web').user
+  public async index({ auth, view }: HttpContextContract) {
+    const user = auth.use('web').user
 
-        // show all apps for every user -> print
-        const apps = await App.query();
+    // show all apps for every user -> print
+    const apps = await App.query();
 
-        for (let i = 0; i < apps.length; i++) {
-            console.log(apps[i]);
-        }
-
-        console.log(user)
-        return view.render('core/dashboard', {
-            user: user,
-        })
-    }
+    return view.render('core/dashboard', {
+      user: user,
+    })
+  }
 }
