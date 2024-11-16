e4f45f22b48ba7e10d9d4c2fe6b82b082e4959a2
Raphael Roumezin <raphael.roumezin@ik.me>
2024-11-16 10:55:30 +0100

    Updated App migration update

--- database/migrations/1731662514979_apps_v2.ts
+++ database/migrations/1731662514979_apps_v2.ts
@@ -3,18 +3,24 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'apps'
 
-  public async up () {
+  public async up() {
     this.schema.alterTable(this.tableName, (table) => {
       table.dropColumns('image', 'releases')
-      table.string('source', 300).nullable()
+      table.string('source', 300)
+
+      table.renameColumn('source_url', 'ext_url')
+      table.setNullable('ext_url')
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.alterTable(this.tableName, (table) => {
       table.string('image', 300).nullable()
       table.string('releases', 300).nullable()
       table.dropColumn('source')
+
+      table.renameColumn('ext_url', 'source_url')
+      table.dropNullable('source_url')
     })
   }
 }
