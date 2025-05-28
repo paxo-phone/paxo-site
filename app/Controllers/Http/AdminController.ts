f5012bc601a0bf6850f451c3b848484a39110855
SuperMax <toto@ok.fr>
2025-05-28 16:13:59 +0200

    fixed: access to admin panel

--- app/Controllers/Http/AdminController.ts
+++ app/Controllers/Http/AdminController.ts
@@ -3,15 +3,8 @@ import { models } from './AdminModelController'
 
 export default class AdminController {
   public async index({ bouncer, view }: HttpContextContract) {
-    await this.checks(bouncer)
-
     return view.render('admin/index', {
       models: Object.keys(models),
     })
-  }
-
-  private async checks(bouncer: HttpContextContract['bouncer']) {
-    if (process.env.UNSAFE_ADMIN_PANEL) return
-    await bouncer.authorize('viewAdminPanel')
   }
 }
