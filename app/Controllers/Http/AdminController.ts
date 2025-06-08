12cde4a77cf77993c6878e4df07cf3ccff40502d
SuperMax <toto@ok.fr>
2025-06-08 19:33:08 +0200

    fixed: space

--- app/Controllers/Http/AdminController.ts
+++ app/Controllers/Http/AdminController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { models } from './AdminModelController'
 
 export default class AdminController {
-  public async index({ bouncer, view }: HttpContextContract) {
+  public async index({ view }: HttpContextContract) {
     return view.render('admin/index', {
       models: Object.keys(models),
     })
