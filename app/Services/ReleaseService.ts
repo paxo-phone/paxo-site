41e19ca1f506b46db7933cdedb5ae55e5efe8632
icimax <supermailmax@proton.me>
2025-08-06 13:34:15 +0200

    fix: resolve conflicts

--- app/Services/ReleaseService.ts
+++ app/Services/ReleaseService.ts
@@ -4,9 +4,6 @@ import Drive from '@ioc:Adonis/Core/Drive'
 import { LocalDriver } from '@adonisjs/core/build/standalone'
 import path from 'path'
 
-import Drive from '@ioc:Adonis/Core/Drive'
-import { LocalDriver } from '@adonisjs/core/build/standalone'
-
 class ReleaseService {
   public async publishRelease(release: Release) {
     await release.load('app')
