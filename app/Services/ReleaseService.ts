2c7f8ac06d1bd291f1af5f91f9ccc364fe09d8ac
icimax <141626084+icimax@users.noreply.github.com>
2025-08-06 11:27:52 +0200

    Updatestore (#92)
    * fixed: unused line
    
    * fixed: unused line
    
    * fixed: unused line
    
    * fixed: unused line
    
    * fixed: unused line
    
    * add: switch of the volumes between dev and prod
    
    * fixed: usage of app.tmp() route to create the folder before moving to the drive
    
    * fixed: usage of app.tmp() route to create the folder before moving to the drive
    
    * fixed: fix routes
    
    * fixed: changed way to handle the storage
    
    * fix: change way to move the release when approved
    
    * fix: change way to public a release

--- app/Services/ReleaseService.ts
+++ app/Services/ReleaseService.ts
@@ -1,19 +1,33 @@
 import Release from 'App/Models/Release'
 import fs from 'fs-extra'
 
+import Drive from '@ioc:Adonis/Core/Drive'
+import { LocalDriver } from '@adonisjs/core/build/standalone'
+
 class ReleaseService {
   public async publishRelease(release: Release) {
     await release.load('app')
     const app = release.app
 
-    const oldAppFilesPath = `apps/${app.uuid}`
-    const newReleaseFilesPath = `releases/${release.uuid}`
-
-    console.log(`Publication de la release ${release.id} pour l'app ${app.id}...`)
-    console.log(`Suppression de l'ancien dossier : ${oldAppFilesPath}`)
-    console.log(`Déplacement du nouveau dossier depuis : ${newReleaseFilesPath}`)
+  
 
     try {
+      const disk = Drive.use()
+      if (!(disk instanceof LocalDriver)) {
+        throw new Error("La publication de release n'est supportée que pour le driver 'local'.")
+      }
+
+      const oldAppDrivePath = `apps/${app.uuid}`
+      const newReleaseDrivePath = `releases/${release.uuid}`
+
+      const oldAppFilesPath = disk.makePath(oldAppDrivePath)
+      const newReleaseFilesPath = disk.makePath(newReleaseDrivePath)
+      
+
+      console.log(`Publication de la release ${release.id} pour l'app ${app.id}...`)
+      console.log(`Suppression de l'ancien dossier : ${oldAppFilesPath}`)
+      console.log(`Déplacement du nouveau dossier depuis : ${newReleaseFilesPath}`)
+
       await fs.remove(oldAppFilesPath)
 
       await fs.move(newReleaseFilesPath, oldAppFilesPath)
